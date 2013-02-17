761ac90259b2342ac96a74dc3a2078e937efd5b1
Ryan Helinski <rlhelinski@gmail.com>
2013-02-17 08:49:49 -0700

    Oh yeah

--- disc-catalog.py
+++ disc-catalog.py
@@ -45,28 +45,7 @@ for (offset, length) in disc.tracks:
 print >> tocf, 'Submit via  :', getSubmissionUrl(disc)
 print 'Submit via  :', getSubmissionUrl(disc)
 
-#tocf.write(getSubmissionUrl(disc))
 tocf.close()
-
-def getXML(query, filter, entity=None, include=None): 
-	if filter is None:
-	    filterParams = [ ]
-	else:
-	    filterParams = filter.createParameters()
-	
-	if include is None:
-	    includeParams = [ ]
-	else:
-	    includeParams = include.createIncludeTags()
-	
-	stream = query._ws.get('release', '', includeParams, filterParams)
-	return stream
-
-#xml = getXML(q, filter)
-#for line in xml:
-#	print line
-
-#xmlf.write(xml.read())
 
 print "Querying MusicBrainz..."
 #print "For more info: http://musicbrainz.org/cdtoc/"+disc.id
@@ -123,7 +102,6 @@ else:
 	print "There were %d matches." % len(results),
 	print "Choose one making you better feeling: "
 	choice = int(sys.stdin.readline().strip())
-	#print "You chose: %d" % choice
 	if choice < 0 or choice >= len(results):
 		print "You failed"
 		sys.exit(1)
@@ -131,16 +109,7 @@ else:
 
 print "Querying MusicBrainz for that release metadata..."
 
-#uuid = mbutils.extractUuid(results[choice].release.getId(), 'release')
-#results_meta = q._getFromWebService('release', uuid)
-#results_meta = q._getFromWebService('release', '', filter=filter)
-#results_meta = q.getReleaseById(results[choice].release.getId())
-# This query seemed to only give me the release title!
-# Instead, replace the list of results with the release chosen
-#results_meta._releaseResults = [ results_meta.releaseResults[choice] ] 
-
 releaseId = mbutils.extractUuid(results_meta.releaseResults[choice].release._id)
-#print "Fetching metadata for", releaseId
 
 if not releaseId:
 	print "It looks like MusicBrainz only has a CD stub for this TOC."
@@ -150,20 +119,5 @@ from catalog import *
 
 c = Catalog()
 c.writeXml(releaseId, c.getReleaseMeta(releaseId))
-
-
-#xmlPath = os.path.join('release-id', disc.id, 'metadata.xml')
-#if (os.path.isfile(xmlPath)):
-	#print xmlPath, "exists. Overwrite? [y/N]"
-	#response = sys.stdin.readline().strip()
-	#if (response != 'y'):
-		#sys.exit(1)
-#
-#print "Writing metadata to", xmlPath
-#xmlf = open(xmlPath, 'w')
-#xml_writer = wsxml.MbXmlWriter()
-#xml_writer.write(xmlf, results_meta)
-#
-#xmlf.close()
 
 # EOF
