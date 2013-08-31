e1a33fa19f17d759818fc9702172458ace762909
Ryan Helinski <rlhelinski@gmail.com>
2013-08-31 16:25:08 -0600

    Wrapping shell into a class

--- catalog-cli.py
+++ catalog-cli.py
@@ -4,229 +4,221 @@ from mbcatalog.catalog import *
 from mbcatalog.extradata import *
 import os, shutil
 
-c = Catalog()
-c.load()
+class Shell:
+    def __init__(self):
+        self.c = Catalog()
+        self.c.load()
+        self.c.report()
 
-def getInput():
-    return sys.stdin.readline().strip()
+    @staticmethod
+    def getInput():
+        return sys.stdin.readline().strip()
 
-def shellSearch():
-    global c
-    while(True):
-        print "Enter search terms (or release ID): ",
-        input = getInput()
-        if input:
-            if len(getReleaseId(input)) == 36:
-                releaseId = getReleaseId(input)
-                c.getSortNeighbors(releaseId, matchFormat=True)
-                return releaseId
-            matches = list(c._search(input))
-            if len(matches) > 1:
-                print len(matches), "matches found:"
-                for i, match in enumerate(matches):
-                    print i, c.formatDiscInfo(match)
-                while (True):
-                    try:
-                        print "Select a match: ",
-                        index = int(getInput())
-                        c.getSortNeighbors(matches[index], matchFormat=True)
-                        return matches[index]
-                    except ValueError as e:
-                        print e, "try again"
-                    except IndexError as e:
-                        print e, "try again"
+    def Search(self):
+        while(True):
+            print "Enter search terms (or release ID): ",
+            input = getInput()
+            if input:
+                if len(getReleaseId(input)) == 36:
+                    releaseId = getReleaseId(input)
+                    self.c.getSortNeighbors(releaseId, matchFormat=True)
+                    return releaseId
+                matches = list(self.c._search(input))
+                if len(matches) > 1:
+                    print len(matches), "matches found:"
+                    for i, match in enumerate(matches):
+                        print i, self.c.formatDiscInfo(match)
+                    while (True):
+                        try:
+                            print "Select a match: ",
+                            index = int(getInput())
+                            self.c.getSortNeighbors(matches[index], matchFormat=True)
+                            return matches[index]
+                        except ValueError as e:
+                            print e, "try again"
+                        except IndexError as e:
+                            print e, "try again"
 
-            elif len(matches) == 1:
-                c.getSortNeighbors(matches[0], matchFormat=True)
-                return matches[0]
+                elif len(matches) == 1:
+                    self.c.getSortNeighbors(matches[0], matchFormat=True)
+                    return matches[0]
+                else:
+                    raise ValueError ("No matches")
             else:
-                raise ValueError ("No matches")
+                raise ValueError ('No release specified')
+
+
+    def Reload(self):
+        del self.c 
+        self.c = Catalog()
+        print "Reloading database...",
+        self.c.load()
+        print "DONE"
+
+    def EditExtra(self):
+        releaseId = shellSearch()
+        ed = ExtraData(releaseId)
+        try:
+            ed.load()
+            print str(ed)
+            print "Modify? [y/N]",
+        except IOError as e:
+            print "Add? [y/N]",
+        modify = getInput()
+        if modify.lower().startswith('y'):
+            ed.interactiveEntry()
+            ed.save()
+
+    def Refresh(self):
+        print "Enter release ID [a for all]: ",
+        releaseId = getReleaseId(getInput())
+        if releaseId == "a":
+            self.c.refreshAllMetaData(60*60)
+        elif releaseId not in self.c:
+            print "Release not found"
+            return
         else:
-            raise ValueError ('No release specified')
+            self.c.refreshMetaData(releaseId, olderThan=60)
 
+    def Change(self):
+        releaseId = shellSearch()
+        print "Enter new release ID: ",
+        newReleaseId = getInput()
+        self.c.renameRelease(releaseId, newReleaseId)
 
-def shellReload():
-    global c # should rap into a class?
-    del c 
-    c = Catalog()
-    print "Reloading database...",
-    c.load()
-    print "DONE"
+    def Html(self):
+        self.c = Catalog()
+        self.c.load()
+        self.c.makeHtml()
 
-def shellEditExtra():
-    global c
-    releaseId = shellSearch()
-    ed = ExtraData(releaseId)
-    try:
-        ed.load()
-        print str(ed)
-        print "Modify? [y/N]",
-    except IOError as e:
-        print "Add? [y/N]",
-    modify = getInput()
-    if modify.lower().startswith('y'):
-        ed.interactiveEntry()
+    def Add(self):
+        print "Enter release ID: ",
+        releaseId = getReleaseId(getInput())
+        if not releaseId:
+            print "No input"
+            return
+        if releaseId in self.c:
+            print "Release already exists"
+            return
+        try:
+            self.c.refreshMetaData(releaseId)
+        except ws.ResourceNotFoundError as e:
+            print "Release not found"
+            return
+        except ws.RequestError as e:
+            print e
+            return
+
+        ed = ExtraData(releaseId)
+        try:
+            ed.load()
+        except IOError as e:
+            "Doesn't matter"
+        ed.addDate()
         ed.save()
 
-def shellRefresh():
-    global c
-    print "Enter release ID [a for all]: ",
-    releaseId = getReleaseId(getInput())
-    if releaseId == "a":
-        c.refreshAllMetaData(60*60)
-    elif releaseId not in c:
-        print "Release not found"
-        return
-    else:
-        c.refreshMetaData(releaseId, olderThan=60)
+    def BarcodeSearch(self):
+        print "Enter barcode: ",
+        barCode = getInput()
+        for releaseId in self.c.barCodeMap[barCode]:
+            print self.c.formatDiscInfo(releaseId)
 
-def shellChange():
-    global c
-    releaseId = shellSearch()
-    print "Enter new release ID: ",
-    newReleaseId = getInput()
-    c.renameRelease(releaseId, newReleaseId)
+    def Delete(self):
+        print "Enter release ID to delete: ",
+        releaseId = getReleaseId(getInput())
+        self.c.deleteRelease(releaseId)
 
-def shellHtml():
-    global c
-    c = Catalog()
-    c.load()
-    c.makeHtml()
+    def Check(self):
+        print "Running checks..."
+        self.c.checkReleases()
+        print "DONE"
 
-def shellAdd():
-    global c
-    print "Enter release ID: ",
-    releaseId = getReleaseId(getInput())
-    if not releaseId:
-        print "No input"
-        return
-    if releaseId in c:
-        print "Release already exists"
-        return
-    try:
-        c.refreshMetaData(releaseId)
-    except ws.ResourceNotFoundError as e:
-        print "Release not found"
-        return
-    except ws.RequestError as e:
-        print e
-        return
+    def Lend(self):
+        releaseId = shellSearch()
+        ed = ExtraData(releaseId)
+        try:
+            ed.load()
+            print str(ed)
+        except IOError as e:
+            pass
 
-    ed = ExtraData(releaseId)
-    try:
-        ed.load()
-    except IOError as e:
-        "Doesn't matter"
-    ed.addDate()
-    ed.save()
+        print "Borrower (leave empty to return): ",
+        borrower = getInput()
+        if not borrower:
+            borrower = '[returned]'
+        print "Lend date (leave empty for today): ",
+        date = getInput()
+        ed.addLend(borrower, date)
+        ed.save()
+        
+    def Path(self):
+        releaseId = shellSearch()
+        ed = ExtraData(releaseId)
+        try:
+            ed.load()
+            print str(ed)
+        except IOError as e:
+            pass
 
-def shellBarcodeSearch():
-    global c
-    print "Enter barcode: ",
-    barCode = getInput()
-    for releaseId in c.barCodeMap[barCode]:
-        print c.formatDiscInfo(releaseId)
-
-def shellDelete():
-    global c
-    print "Enter release ID to delete: ",
-    releaseId = getReleaseId(getInput())
-    c.deleteRelease(releaseId)
-
-def shellCheck():
-    global c
-    print "Running checks..."
-    c.checkReleases()
-    print "DONE"
-
-def shellLend():
-    global c
-    releaseId = shellSearch()
-    ed = ExtraData(releaseId)
-    try:
-        ed.load()
-        print str(ed)
-    except IOError as e:
-        pass
-
-    print "Borrower (leave empty to return): ",
-    borrower = getInput()
-    if not borrower:
-        borrower = '[returned]'
-    print "Lend date (leave empty for today): ",
-    date = getInput()
-    ed.addLend(borrower, date)
-    ed.save()
-    
-def shellPath():
-    global c
-    releaseId = shellSearch()
-    ed = ExtraData(releaseId)
-    try:
-        ed.load()
-        print str(ed)
-    except IOError as e:
-        pass
-
-    print "Enter path: ",
-    path = getInput()
-    if path.startswith("'") and path.endswith("'"):
-        path = path[1:-1]
-    ed.addPath(path)
-    ed.save()
-
-shellCommands = {
-    'h' : (None, 'this help'),
-    'q' : (None, 'quit'),
-    'e' : (shellEditExtra, 'edit extra data'),
-    's' : (shellSearch, 'search for releases'),
-    'r' : (shellRefresh, 'refresh XML metadata from musicbrainz'),
-    't' : (shellHtml, 'write hTml'),
-    'c' : (shellChange, 'change release'),
-    'a' : (shellAdd, 'add release'),
-    'l' : (shellReload, 'reLoad database from disk'),
-    'b' : (shellBarcodeSearch, 'barcode search'),
-    'd' : (shellDelete, 'delete release'),
-    'k' : (shellCheck, 'check releases'),
-    'n' : (shellLend, 'leNd (checkout) release'),
-    'p' : (shellPath, 'add Path to release'),
-    }
+        print "Enter path: ",
+        path = getInput()
+        if path.startswith("'") and path.endswith("'"):
+            path = path[1:-1]
+        ed.addPath(path)
+        ed.save()
 
 
+    shellCommands = {
+        'h' : (None, 'this help'),
+        'q' : (None, 'quit'),
+        'e' : (EditExtra, 'edit extra data'),
+        's' : (Search, 'search for releases'),
+        'r' : (Refresh, 'refresh XML metadata from musicbrainz'),
+        't' : (Html, 'write hTml'),
+        'c' : (Change, 'change release'),
+        'a' : (Add, 'add release'),
+        'l' : (Reload, 'reLoad database from disk'),
+        'b' : (BarcodeSearch, 'barcode search'),
+        'd' : (Delete, 'delete release'),
+        'k' : (Check, 'check releases'),
+        'n' : (Lend, 'leNd (checkout) release'),
+        'p' : (Path, 'add Path to release'),
+        }
 
-def commandShell():
-    global c
-    while (True):
-        print "Enter command ('h' for help): ",
-        input = getInput().lower()
+    def main(self):
+        while (True):
+            print "Enter command ('h' for help): ",
+            input = getInput().lower()
 
-        if not input or input.startswith('q'):
-            break
+            if not input or input.startswith('q'):
+                break
 
-        if (input.startswith('h')):
-            print "\r",
-            #for letter, descr in shellCommands.items():
-            for letter in sorted(shellCommands.keys()):
-                print letter + " : " + shellCommands[letter][1]
+            if (input.startswith('h')):
+                print "\r",
+                for letter in sorted(self.shellCommands.keys()):
+                    print letter + " : " + self.shellCommands[letter][1]
 
-        elif input[0] in shellCommands.keys():
-            print shellCommands[input[0]][1]
-            # Call the function
-            try:
-                (shellCommands[input[0]][0])()
-            except ValueError as e:
-                print e
+            elif input[0] in self.shellCommands.keys():
+                print self.shellCommands[input[0]][1]
+                # Call the function
+                try:
+                    (self.shellCommands[input[0]][0])(self)
+                except ValueError as e:
+                    print e
 
-        else:
-            print "Invalid command"
+            else:
+                print "Invalid command"
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         search_terms = sys.argv
         del search_terms[0]
+        c = Catalog()
+        c.load()
         c.search(' '.join(search_terms))
-    else:
-        c.report()
 
-    commandShell()
+    else:
+        s = Shell()
+        s.main()
+
