b46cfd953b274129eb636f49ff881bafaa956ea8
Ryan Helinski <rlhelinski@gmail.com>
2013-11-08 19:54:33 -0700

    Rewrite of disc-catalog.py to drop dependency on musicbrainz2 and replace with python-discid. Had to re-write the getSubmissionUrl() function from musicbrainz2.disc to work with discid. Added this function to mbcat.utils, though perhaps it belongs in discid.

--- disc-catalog.py
+++ disc-catalog.py
@@ -1,19 +1,26 @@
 #!/opt/local/bin/python2.7
 #
-# Read a CD in the disc drive and calculate a MusicBrainz DiscID.
+# Read a CD in the disc drive, compute the MusicBrainz DiscID,
+# find a MusicBrainz release, and add that to your catalog. 
 #
 # Usage:
-#       python discid.py [/path/to/cdrom]
+#       python disc-catalog.py [/path/to/cdrom]
 #
 # $Id$
 #
+from __future__ import print_function
 import sys, os
-from musicbrainz2.disc import readDisc, getSubmissionUrl, DiscError
-import musicbrainz2.webservice as ws
-import musicbrainz2.wsxml as wsxml
-import musicbrainz2.utils as mbutils
+import discid
+import musicbrainzngs as mb
 from mbcat.catalog import *
 from mbcat.extradata import *
+from mbcat.utils import *
+
+mb.set_useragent(
+    "python-musicbrainz-ngs-catalog",
+    "0.1",
+    "https://github.com/rlhelinski/musicbrainz-catalog/",
+)
 
 c = Catalog()
 c.load()
@@ -24,102 +31,98 @@ while not input.startswith('q'):
         # Read the disc in the default disc drive. If necessary, you can pass
         # the 'deviceName' parameter to select a different drive.
         #
-        if len(sys.argv) > 1:
-            disc = readDisc(deviceName=sys.argv[1])
-        else:
-            disc = readDisc()
+        dev = sys.argv[1] if len(sys.argv) > 1 else discid.get_default_device()
+        print ('Reading from %s' % dev)
+        disc = discid.read(dev)
     except DiscError, e:
-        print "DiscID calculation failed:", str(e)
+        print ("DiscID calculation failed:", str(e))
         sys.exit(1)
 
+    # This should be replaced with a log output 
     if (not os.path.isdir('disc-id')):
         os.mkdir('disc-id')
     if (not os.path.isdir(os.path.join('disc-id', disc.id))):
         os.mkdir(os.path.join('disc-id', disc.id))
 
-    tocf = open(os.path.join('disc-id', disc.id, 'toc.txt'), 'a')
-    print >> tocf, 'DiscID      :', disc.id
-    print 'DiscID      :', disc.id
-    print >> tocf, 'First Track :', disc.firstTrackNum
-    print >> tocf, 'Last Track  :', disc.lastTrackNum
-    print >> tocf, 'Length      :', disc.sectors, 'sectors'
+    with open(os.path.join('disc-id', disc.id, 'toc.txt'), 'a') as tocf:
+        print ('DiscID      :', disc.id, file=tocf)
+        print ('First Track :', disc.first_track_num, file=tocf)
+        print ('Last Track  :', disc.last_track_num, file=tocf)
+        print ('Length      :', disc.sectors, 'sectors', file=tocf)
 
-    i = disc.firstTrackNum
-    for (offset, length) in disc.tracks:
-        print >> tocf, "Track %-2d    : %8d %8d" % (i, offset, length)
-        i += 1
+        i = disc.first_track_num
+        for track in disc.tracks:
+            print ("Track %-2d    : %8d %8d" % (i, track.offset, track.length), file=tocf)
+            i += 1
 
-    print >> tocf, 'Submit via  :', getSubmissionUrl(disc)
-    print 'Submit via  :', getSubmissionUrl(disc)
+        # TODO fix this
+        print ('Submit via  :', getSubmissionUrl(disc), file=tocf)
+        print ('Submit via  :', getSubmissionUrl(disc))
 
-    tocf.close()
+    print ('DiscID:', disc.id)
+    
+    print ("Querying MusicBrainz...")
+    try:
+        result = mb.get_releases_by_discid(disc.id,
+                includes=["artists"])
+    except mb.ResponseError:
+        print("disc not found or bad response")
+    else:
+        if result.get("disc"):
+            for i, rel in enumerate(result['disc']['release-list']):
+                print ("\nResult : %d" % i)
+                #if rel.score:
+                    #print " Score   : %d" % rel.score
+                print ("Release  :", rel['id'])
+                print ("Artist   :", rel['artist-credit-phrase'])
+                print ("Title    :", rel['title'])
+                print ("Date    :", rel['date'])
+                print ("Country    :", rel['country'])
+                print ("Barcode    :", rel['barcode'])
+                if 'label-info-list' in rel:
+                    for label_info in rel['label-info-list']:
+                        for label, field in [ \
+                                        ("Label:", rel['label']['name']), \
+                                        ("Catalog #:", rel['catalog-number']), \
+                                        ("Barcode :", releaseEvent.barcode) ]:
+                            if field:
+                                print (label, field, ",\t")
+                            else:
+                                print (label, "\t,\t")
+                print ()
 
-    print "Querying MusicBrainz..."
-    #print "For more info: http://musicbrainz.org/cdtoc/"+disc.id
-    q = ws.Query()
-    filter = ws.ReleaseFilter(discId=disc.id)
-    #results = q.getReleases(filter=filter)
-    results_meta = q._getFromWebService('release', '', filter=filter,
-                    include=ws.ReleaseIncludes(
-                            artist=True,
-                            counts=True,
-                            releaseEvents=True,
-                            discs=True,
-                            labels=True,
-                            tracks=True,
-                            tags=True,
-                            #ratings=True,
-                            #isrcs=True
-                            ))
-    results = results_meta.getReleaseResults()
+        elif result.get("cdstub"):
+            print ('We found only a stub')
+            print("artist:\t" % result["cdstub"]["artist"])
+            print("title:\t" % result["cdstub"]["title"])
+            raise
 
-    for i, result in enumerate(results):
-        print "\nResult : %d" % i,
-        if result.score:
-            print " Score   : %d" % result.score
-        print "Release  :", result.release._id
-        print "Artist   :", result.release.artist.getName()
-        print "Title    :", result.release.title
-        for releaseEvent in result.release.releaseEvents:
-            for label, field in [("Released :", releaseEvent.date), \
-                            ("Country       :", releaseEvent.country), \
-                            ("Label :", (releaseEvent.label.name if releaseEvent.label else None)), \
-                            ("Catalog #:", releaseEvent.catalogNumber), \
-                            ("Barcode :", releaseEvent.barcode) ]:
-                if field:
-                    print label, field, ",\t",
-                else:
-                    print label, "\t,\t",
-        print
-        if len(result.release.releaseEvents):
-            print "Date     :", result.release.releaseEvents[0].getDate()
-
-    if len(results) == 0:
-        print "There were no matches!"
-        print "Enter a note to yourself: "
+    if len(result['disc']['release-list']) == 0:
+        print ("There were no matches!")
+        print ("Enter a note to yourself: ")
         note = sys.stdin.readline()
         tocf = open(os.path.join('disc-id', disc.id, 'toc.txt'), 'a')
         tocf.write(note)
         tocf.close()
         sys.exit(1)
-    elif len(results) == 1:
-        print "There was one match."
+    elif len(result['disc']['release-list']) == 1:
+        print ("There was one match.")
         choice = 0
     else:
-        print "There were %d matches." % len(results),
-        print "Choose one making you better feeling: "
+        print ("There were %d matches." % len(result['disc']['release-list']))
+        print ("Choose one making you better feeling: ")
         choice = int(sys.stdin.readline().strip())
-        if choice < 0 or choice >= len(results):
-            print "You failed"
+        if choice < 0 or choice >= len(result['disc']['release-list']):
+            print ("You failed")
             sys.exit(1)
 
 
-    print "Querying MusicBrainz for that release metadata..."
+    print ("Adding that release to the catalog...")
 
-    releaseId = mbutils.extractUuid(results_meta.releaseResults[choice].release._id)
+    releaseId = extractUuid(result['disc']['release-list'][choice]['id'])
 
     if not releaseId:
-        print "It looks like MusicBrainz only has a CD stub for this TOC."
+        print ("It looks like MusicBrainz only has a CD stub for this TOC.")
         sys.exit(1)
 
     c.refreshMetaData(releaseId)
