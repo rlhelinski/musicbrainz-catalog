002e07692b09e23c05ef44c5512064097706f9c6
Ryan Helinski <rlhelinski@gmail.com>
2013-12-07 22:44:47 -0700

    xrange() does not exist in Python3
    Signed-off-by: Ryan Helinski <rlhelinski@gmail.com>

--- mbcat/catalog.py
+++ mbcat/catalog.py
@@ -43,7 +43,7 @@ def releaseSortCmp(a, b):
 
 def chunks(l, n):
     """ Yield successive n-sized chunks from l. """
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i+n]
 
 # This goes with getFormatFromUri(), but can we replace that with a library function?
