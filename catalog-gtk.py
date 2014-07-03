6e787b025b39015944208a9e9d1d145a11e68885
Ryan Helinski <rlhelinski@gmail.com>
2014-07-03 00:42:38 -0600

    A rudimentary start with a GTK TreeView.

---
+++ catalog-gtk.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+#
+#
+
+from __future__ import print_function
+import logging
+logging.basicConfig(level=logging.INFO)
+import pygtk
+pygtk.require('2.0')
+import gtk
+import mbcat
+import argparse
+
+class MBCatGtk:
+    columnNames = ['Artist', 'Release Title', 'Date', 'Country', 'Label',
+        'Catalog #', 'Barcode', 'ASIN']
+    numFields = ['Barcode', 'ASIN']
+
+    # This is a callback function. The data arguments are ignored
+    # in this example. More on callbacks below.
+    def hello(self, widget, data=None):
+        print ("Hello World")
+
+    def delete_event(self, widget, event, data=None):
+        # If you return FALSE in the "delete_event" signal handler,
+        # GTK will emit the "destroy" signal. Returning TRUE means
+        # you don't want the window to be destroyed.
+        # This is useful for popping up 'are you sure you want to quit?'
+        # type dialogs.
+        print ("delete event occurred")
+
+        # Change FALSE to TRUE and the main window will not be destroyed
+        # with a "delete_event".
+        return False
+
+    def destroy(self, widget, data=None):
+        print ("destroy signal occurred")
+        gtk.main_quit()
+
+    def openAboutWindow(self, widget):
+        about = gtk.AboutDialog()
+        about.set_program_name(progname)
+        about.set_version(progver)
+        about.set_copyright(progcopy)
+        about.set_comments(progcomm)
+        about.set_website(progurl)
+        about.set_logo(pumppb)
+        about.run()
+        about.destroy()
+        return
+
+    def format_comment(self, column, cell, model, it, field):
+        row = model.get_value(it, 0)
+        cell.set_property('text', 'OK')
+
+    def editrecord(self, tree, path, column):
+        pass
+        self.createEditWindow(self.get_current_row())
+
+    def on_row_select(self, widget):
+        pass
+
+    def __init__(self, catalog):
+        self.catalog = catalog
+
+        # create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title('mbcat')
+        self.window.set_size_request(800, 600)
+        self.window.set_position(gtk.WIN_POS_CENTER)
+    
+        # When the window is given the "delete_event" signal (this is given
+        # by the window manager, usually by the "close" option, or on the
+        # titlebar), we ask it to call the delete_event () function
+        # as defined above. The data passed to the callback
+        # function is NULL and is ignored in the callback function.
+        self.window.connect("delete_event", self.delete_event)
+    
+        # Here we connect the "destroy" event to a signal handler.  
+        # This event occurs when we call gtk_widget_destroy() on the window,
+        # or if we return FALSE in the "delete_event" callback.
+        self.window.connect("destroy", self.destroy)
+    
+        # Sets the border width of the window.
+        self.window.set_border_width(10)
+    
+        # Menu bar
+        mb = gtk.MenuBar()
+        # File menu
+        filemenu = gtk.Menu()
+        filem = gtk.MenuItem("_File")
+        filem.set_submenu(filemenu)
+
+        mb.append(filem)
+
+        # ...
+                # Help menu
+        helpmenu = gtk.Menu()
+        help = gtk.MenuItem("Help")
+        help.set_submenu(helpmenu)
+
+        aboutm = gtk.MenuItem("About")
+        aboutm.connect("activate", self.openAboutWindow)
+        helpmenu.append(aboutm)
+
+        mb.append(help)
+
+        # create the TreeView
+        self.treeview = gtk.TreeView()
+        # TODO add ability to sort by headers
+        self.treeview.set_headers_clickable(True)
+        # rules-hint
+        self.treeview.set_rules_hint(True);
+
+        vbox = gtk.VBox(False, 2)
+        vbox.pack_start(mb, False, False, 0)
+
+        # create the TreeViewColumns to display the data
+        self.tvcolumn = [None] * len(self.columnNames)
+        for n, columnName in enumerate(self.columnNames):
+            cell = gtk.CellRendererText()
+            if (columnName in self.numFields):
+                cell.set_property('xalign', 1.0)
+            self.tvcolumn[n] = gtk.TreeViewColumn(columnName, cell)
+            self.tvcolumn[n].set_cell_data_func(cell, self.format_comment,
+                columnName)
+            self.tvcolumn[n].set_resizable(True)
+            self.treeview.append_column(self.tvcolumn[n])
+
+        self.treeview.connect('row-activated', self.editrecord)
+        self.treeview.connect('cursor-changed', self.on_row_select)
+        self.scrolledwindow = gtk.ScrolledWindow()
+        self.scrolledwindow.add(self.treeview)
+        vbox.pack_start(self.scrolledwindow, True, True, 0)
+
+        self.releaseList = gtk.ListStore(object)
+        for i, relId in enumerate(self.catalog.getReleaseIds()):
+            self.releaseList.append([i])
+        self.treeview.set_model(self.releaseList)
+
+        # Creates a new button with the label "Hello World".
+        self.button = gtk.Button("Hello World")
+    
+        # When the button receives the "clicked" signal, it will call the
+        # function hello() passing it None as its argument.  The hello()
+        # function is defined above.
+        self.button.connect("clicked", self.hello, None)
+    
+        # This will cause the window to be destroyed by calling
+        # gtk_widget_destroy(window) when "clicked".  Again, the destroy
+        # signal could come from here, or the window manager.
+        self.button.connect_object("clicked", gtk.Widget.destroy, self.window)
+    
+        # This packs the button into the window (a GTK container).
+        vbox.pack_start(self.button, False, False, 0)
+
+        self.window.add(vbox)
+    
+        # The final step is to display this newly created widget.
+        # and the window
+        self.window.show_all()
+
+    def main(self):
+        # All PyGTK applications must have a gtk.main(). Control ends here
+        # and waits for an event to occur (like a key press or mouse event).
+        gtk.main()
+
+# If the program is run directly or passed as an argument to the python
+# interpreter then create a GtkGui instance and show it
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Runs the MusicBrainz-Catalog shell')
+    parser.add_argument('--database', help='Specify the path to the catalog database')
+    parser.add_argument('--cache', help='Specify the path to the file cache')
+    args = parser.parse_args()
+
+    c = mbcat.catalog.Catalog(dbPath=args.database, cachePath=args.cache)
+    gui = MBCatGtk(catalog=c)
+    gui.main()
