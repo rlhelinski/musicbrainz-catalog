c938d46d7b924b2efa073efdf14b43eb6ef0f90d
Ryan Helinski <rlhelinski@gmail.com>
2014-09-10 21:06:01 -0600

    Restructuring this reference code in order to try a new approach. It requires two threads, but this will allow the actual work function to be separated from the GUI code.

--- progressbar-reference.py
+++ progressbar-reference.py
@@ -1,104 +1,108 @@
 import threading
+from threading import Thread
 import random, time
 import gobject
 import gtk
 #Initializing the gtk's thread engine
 gobject.threads_init()
 
-def DummyTask():
-    """
-    This task does nothing at all. It demonstrates how to create a generator
-    for running tasks in a progress dialog with TaskHandler.
-    """
-    while True:
-        time.sleep(0.1)
-        yield random.random()
+window = None
 
-def ExampleTask1():
-    """
-    Count from 0 to 99.
-    """
-    N = 100
-    for i in range(N):
-        time.sleep(0.1)
-        yield float(i)/N
-    yield False
+class ProgressDialog(threading.Thread):
+    """This does something that takes a while and keeps track of its own
+    progress"""
+    
+    def __init__(self, task):
+        super(ProgressDialog, self).__init__()
+        self.task = task
 
-def ExampleTask2():
-    """
-    Pretend that we don't know how much work there is to do and just indicate
-    that something is happening.
-    """
-    N = 100
-    for i in range(N):
-        time.sleep(0.1)
-        yield True
-    yield False
+        gtk.gdk.threads_enter()
+        self.pbarwindow = gtk.Window()
+        self.pbarwindow.set_transient_for(window)
+        self.progressbar = gtk.ProgressBar()
+        self.pbarwindow.add(self.progressbar)
+        self.pbarwindow.show_all()
+        gtk.gdk.threads_leave()
 
-class TaskHandler(threading.Thread):
-    """This class sets the fraction of the progressbar"""
+    def run(self):
+        """Run method, this is the code that runs while thread is alive."""
+        self.task.start()
+
+        while not self.task.stopthread.isSet():
+            gtk.gdk.threads_enter()
+            self.progressbar.set_fraction(
+                float(self.task.numer)/self.task.denom)
+            self.progressbar.set_text(str(time.time()))
+            gtk.gdk.threads_leave()
+            time.sleep(0.1)
+        self.quit()
+
+    def quit(self):
+        self.pbarwindow.destroy()
+
+    def stop(self):
+        """Stop method, sets the event to terminate the thread's main loop"""
+        self.task.stop()
+        self.quit()
+
+class LongTask(threading.Thread):
+    """This does something that takes a while and keeps track of its own
+    progress"""
     
     #Thread event, stops the thread if it is set.
     stopthread = threading.Event()
 
-    def __init__(self, task_generator):
-        super(TaskHandler, self).__init__()
-        self.task_generator = task_generator
-
-        #Gui bootstrap: window and progressbar
-        self.window = gtk.Window()
-        self.progressbar = gtk.ProgressBar()
-        self.window.add(self.progressbar)
-        self.window.show_all()
-        #Connecting the 'destroy' event to the main_quit function
-        self.window.connect('destroy', main_quit)
+    def __init__(self, denom):
+        super(LongTask, self).__init__()
+        self.denom = denom
+        self.numer = 0
 
     def run(self):
         """Run method, this is the code that runs while thread is alive."""
 
         #While the stopthread event isn't setted, the thread keeps going on
         while not self.stopthread.isSet():
-            r = self.task_generator.next()
-            if isinstance(r, bool) and not r:
-                self.window.destroy()
+            if self.numer > self.denom:
+                self.stopthread.set()
                 break
-            # Acquiring the gtk global mutex
-            gtk.threads_enter()
-            #Setting a random value for the fraction
-            if isinstance(r, float):
-                self.progressbar.set_fraction(r)
-            elif isinstance(r, bool):
-                self.progressbar.pulse()
-            else:
-                gtk.threads_leave()
-                raise ValueError('Unsupported type yielded by generator')
-            # Releasing the gtk global mutex
-            gtk.threads_leave()
+            time.sleep(1)
+            self.numer += 1
+
+        self.stopthread.set()
 
     def stop(self):
         """Stop method, sets the event to terminate the thread's main loop"""
         self.stopthread.set()
-        self.window.destroy()
 
 def main_quit(obj):
     """main_quit function, it stops the thread and the gtk's main loop"""
     #Importing the fs object from the global scope
     global th
     #Stopping the thread and the gtk's main loop
-    th.stop()
+    #th.stop()
     gtk.main_quit()
 
+def run_long_task(widget):
+    t = ProgressDialog(LongTask(10))
+    t.start()
 
-#Creating and starting the thread
-#th = TaskHandler(DummyTask())
-#th = TaskHandler(ExampleTask1())
-th = TaskHandler(ExampleTask2())
-th.start()
+window = gtk.Window()
+vbox = gtk.VBox()
+label = gtk.Label('Hello, world!')
+vbox.pack_start(label)
+button = gtk.Button('Come on, do it!')
+button.connect('clicked', run_long_task)
+vbox.pack_start(button)
+window.add(vbox)
+window.connect('destroy', main_quit)
+window.show_all()
 
-print threading.enumerate()
+# TODO show this result in a Label
+#print threading.enumerate()
 
 try:
     gtk.main()
 except KeyboardInterrupt:
+    pass
     # Kill the thread
-    th.stop()
+    #th.stop()
