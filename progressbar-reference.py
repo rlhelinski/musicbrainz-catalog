38e7153459aa51e5a7764b1dc7398a43f27f18e5
Ryan Helinski <rlhelinski@gmail.com>
2014-09-12 08:20:21 -0600

    Adding code for functions which are not designed to be used with these progress bars.

--- progressbar-reference.py
+++ progressbar-reference.py
@@ -7,6 +7,17 @@ gobject.threads_init()
 
 window = None
 
+# 0.04 = 25fps
+# 0.041 = 24fps
+# 0.055 = 18fps
+# 0.083 = 12fps
+# 0.125 = 8fps
+# 0.166 = 6fps
+
+
+def much_fun(arg):
+    time.sleep(arg)
+
 class ProgressDialog(threading.Thread):
     """This does something that takes a while and keeps track of its own
     progress"""
@@ -43,6 +54,34 @@ class ProgressDialog(threading.Thread):
         """Stop method, sets the event to terminate the thread's main loop"""
         self.task.stop()
         self.quit()
+
+class PulseDialog(ProgressDialog):
+    def run(self):
+        """Start the sub-thread; pulse the progress bar until it finishes"""
+        self.task.start()
+
+        tstart = time.time()
+        while self.task.isAlive():
+            gtk.gdk.threads_enter()
+            self.progressbar.pulse()
+            self.progressbar.set_text(str(time.time() - tstart))
+            gtk.gdk.threads_leave()
+            time.sleep(0.1)
+        self.quit()
+
+class ThreadedCall(threading.Thread):
+    """This simply runs a function in a new thread"""
+
+    def __init__(self, fun, *args):
+        super(ThreadedCall, self).__init__()
+        self.fun = fun
+        self.args = args
+
+    def run(self):
+        """Run method, this is the code that runs while thread is alive.
+        This is just an example. You can make your own by creating a class
+        that inherits this class and defining your own run() method."""
+        self.result = self.fun(*self.args)
 
 class ThreadedTask(threading.Thread):
     """This does something that takes a while and keeps track of its own
@@ -106,8 +145,13 @@ def run_long_task(widget):
 def run_mutex_task(widget):
     t = ProgressDialog(MutexTask(10)).start()
 
+def run_pulse_task(widget):
+    t = PulseDialog(ThreadedCall(much_fun, 10)).start()
+
 window = gtk.Window()
-vbox = gtk.VBox()
+window.set_has_frame(True)
+window.set_frame_dimensions(10, 10, 10, 10)
+vbox = gtk.VBox(False, 10)
 label = gtk.Label('Hello, world!')
 vbox.pack_start(label)
 button = gtk.Button('Come on, do it!')
@@ -115,6 +159,9 @@ button.connect('clicked', run_long_task)
 vbox.pack_start(button)
 button = gtk.Button('Easy with this one')
 button.connect('clicked', run_mutex_task)
+vbox.pack_start(button)
+button = gtk.Button('This one is touchy!')
+button.connect('clicked', run_pulse_task)
 vbox.pack_start(button)
 window.add(vbox)
 window.connect('destroy', main_quit)
