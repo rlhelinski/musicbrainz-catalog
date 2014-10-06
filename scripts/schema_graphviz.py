4f6d89b27bc7673df2a3bdf9b789c8f18caf1d9f
Ryan Helinski <rlhelinski@gmail.com>
2014-10-06 08:51:27 -0600

    Adding script to render a graph of the database schema and the output PNG of that script for documentation.

---
+++ scripts/schema_graphviz.py
@@ -0,0 +1,70 @@
+import shlex
+
+"""
+A script to convert an SQL schema into a GraphViz diagram.
+"""
+
+dot_lines = ["""digraph schema {
+    rankdir="BT";
+    node [shape=plaintext]
+""",
+    ]
+edge_lines = []
+
+with open('mbcat.schema', 'r') as f:
+    for line in f:
+        lexer = shlex.shlex(line)
+        if lexer.get_token().lower() == 'create':
+            if lexer.get_token().lower() == 'table':
+                table_name = lexer.get_token()
+                dot_lines.append("""    %s[label=<
+<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+    <TR><TD BORDER="0" COLSPAN="20" ALIGN="left">%s</TD></TR>
+    <TR>""" % (table_name, table_name))
+                assert lexer.get_token() == '('
+                while True:
+                    tok = lexer.get_token()
+                    if tok.lower() == 'foreign':
+                        assert lexer.get_token().lower() == 'key'
+                        assert lexer.get_token() == '('
+                        foreign_child_column = lexer.get_token()
+                        assert lexer.get_token() == ')'
+                        assert lexer.get_token().lower() == 'references'
+                        foreign_parent_table = lexer.get_token()
+                        assert lexer.get_token() == '('
+                        foreign_parent_column = lexer.get_token()
+                        assert lexer.get_token() == ')'
+
+                        edge_lines.append("""    %s:%s -> %s:%s;""" % \
+                            (table_name, foreign_child_column,
+                            foreign_parent_table, foreign_parent_column))
+
+                        foreign_options = []
+                        tok = lexer.get_token()
+                        while tok != ';' and tok != ',':
+                            foreign_options.append(tok)
+                            tok = lexer.get_token()
+                    elif tok != '':
+                        column_name = tok
+                        dot_lines.append('        <TD PORT="%s">%s</TD>'%\
+                            (column_name, column_name))
+                        column_options = []
+                        tok = lexer.get_token()
+                        while tok != ',' and tok != ')':
+                            column_options.append(tok)
+                            tok = lexer.get_token()
+                        if tok == ')':
+                            tok = lexer.get_token()
+                            assert tok == ';'
+                            break
+                    else:
+                        break
+
+                dot_lines.append("""    </TR>
+</TABLE>>];""")
+
+dot_lines.extend(edge_lines)
+dot_lines.append('}')
+
+with open('schema.dot', 'w') as outf:
+    outf.writelines('\n'.join(dot_lines))
