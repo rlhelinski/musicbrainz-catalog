c035cc0a65871197237c0e70ec6154eaf09c4bfc
Ryan Helinski <rlhelinski@gmail.com>
2014-10-07 22:42:10 -0600

    Parsing into a data structure before rendering DOT code. Still writing edge lines while parsing.

--- scripts/schema_graphviz.py
+++ scripts/schema_graphviz.py
@@ -1,4 +1,5 @@
 import shlex
+from collections import OrderedDict
 
 """
 A script to convert an SQL schema into a GraphViz diagram.
@@ -12,55 +13,69 @@ digraph schema {
     ]
 edge_lines = []
 
+tables = list()
+
 with open('schema.sql', 'r') as f:
     for line in f:
         lexer = shlex.shlex(line)
         if lexer.get_token().lower() == 'create':
             if lexer.get_token().lower() == 'table':
-                table_name = lexer.get_token()
-                dot_lines.append("""    %s[label=<
-<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
-    <TR><TD BORDER="0" COLSPAN="20" ALIGN="left">%s</TD></TR>
-    <TR>""" % (table_name, table_name))
+                table = dict()
+                tables.append(table)
+                table['name'] = lexer.get_token()
+                table['columns'] = OrderedDict()
+
                 assert lexer.get_token() == '('
                 while True:
                     tok = lexer.get_token()
                     if tok.lower() == 'foreign':
                         assert lexer.get_token().lower() == 'key'
                         assert lexer.get_token() == '('
-                        foreign_child_column = lexer.get_token()
+                        child_column = lexer.get_token()
+                        col = table['columns'][child_column]
+                        col['foreign-key'] = dict()
                         assert lexer.get_token() == ')'
                         assert lexer.get_token().lower() == 'references'
-                        foreign_parent_table = lexer.get_token()
+                        col['foreign-key']['parent-table'] = lexer.get_token()
                         assert lexer.get_token() == '('
-                        foreign_parent_column = lexer.get_token()
+                        col['foreign-key']['parent-column'] = lexer.get_token()
                         assert lexer.get_token() == ')'
 
                         edge_lines.append("""    %s:%s:n -> %s:%s:s;""" % \
-                            (table_name, foreign_child_column,
-                            foreign_parent_table, foreign_parent_column))
+                            (table['name'], child_column,
+                            col['foreign-key']['parent-table'],
+                            col['foreign-key']['parent-column']))
 
-                        foreign_options = []
+                        col['foreign-key']['options'] = list()
                         tok = lexer.get_token()
                         while tok != ';' and tok != ',':
-                            foreign_options.append(tok)
+                            col['foreign-key']['options'].append(tok)
                             tok = lexer.get_token()
                     elif tok.lower() == 'primary':
                         assert lexer.get_token().lower() == 'key'
+                        assert lexer.get_token() == '('
+                        while True:
+                            col = table['columns'][lexer.get_token()]
+                            col['primary-key'] = True
+                            tok = lexer.get_token()
+                            if tok != ',':
+                                break
+                        assert tok == ')'
+                        #tok = lexer.get_token()
+                        #if tok == '(':
+                            #while tok != ')':
+                                #tok = lexer.get_token()
                         tok = lexer.get_token()
-                        if tok == '(':
-                            while tok != ')':
-                                tok = lexer.get_token()
-                        tok = lexer.get_token()
-                        assert tok == ',' or tok == ')'
+                        #assert tok == ',' or tok == ')'
                     elif tok != '':
-                        column_name = tok
-                        dot_lines.append('        <TD PORT="%s">%s</TD>'%\
-                            (column_name, column_name))
-                        column_options = []
+                        col = dict(options=[])
+                        table['columns'][tok] = col
                         tok = lexer.get_token()
                         while tok != ',' and tok != ')':
-                            column_options.append(tok)
+                            if tok.lower() == 'primary':
+                                assert lexer.get_token().lower() == 'key'
+                                col['primary-key'] = True
+                            col['options'].append(tok)
                             tok = lexer.get_token()
                         if tok == ')':
                             tok = lexer.get_token()
@@ -69,7 +84,15 @@ with open('schema.sql', 'r') as f:
                     else:
                         break
 
-                dot_lines.append("""    </TR>
+for table in tables:
+    dot_lines.append("""    %s[label=<
+<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+    <TR><TD BORDER="0" COLSPAN="20" ALIGN="left">%s</TD></TR>
+    <TR>""" % (table['name'], table['name']))
+    for column_name, col in table['columns'].items():
+        dot_lines.append('        <TD PORT="%s">%s%s</TD>'%\
+            (column_name, column_name, '*' if 'primary-key' in col else ''))
+    dot_lines.append("""    </TR>
 </TABLE>>];""")
 
 dot_lines.extend(edge_lines)
