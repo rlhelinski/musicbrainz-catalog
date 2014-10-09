ca1d3229069a4feecb9bb32417d1ce360f03ed3c
Ryan Helinski <rlhelinski@gmail.com>
2014-10-08 23:17:39 -0600

    Updating schema doc

--- scripts/schema_graphviz.py
+++ scripts/schema_graphviz.py
@@ -16,73 +16,78 @@ edge_lines = []
 tables = list()
 
 with open('schema.sql', 'r') as f:
-    for line in f:
-        lexer = shlex.shlex(line)
-        if lexer.get_token().lower() == 'create':
-            if lexer.get_token().lower() == 'table':
-                table = dict()
-                tables.append(table)
-                table['name'] = lexer.get_token()
-                table['columns'] = OrderedDict()
+    stmts = f.read()
 
-                assert lexer.get_token() == '('
-                while True:
+stmts = ' '.join(stmts.split('\n'))
+
+for stmt in stmts.split(';'):
+    stmt += ';'
+    lexer = shlex.shlex(stmt)
+    if lexer.get_token().lower() == 'create':
+        if lexer.get_token().lower() == 'table':
+            table = dict()
+            tables.append(table)
+            table['name'] = lexer.get_token()
+            table['columns'] = OrderedDict()
+
+            assert lexer.get_token() == '('
+            while True:
+                tok = lexer.get_token()
+                if tok.lower() == 'foreign':
+                    assert lexer.get_token().lower() == 'key'
+                    assert lexer.get_token() == '('
+                    child_column = lexer.get_token()
+                    col = table['columns'][child_column]
+                    col['foreign-key'] = dict()
+                    assert lexer.get_token() == ')'
+                    assert lexer.get_token().lower() == 'references'
+                    col['foreign-key']['parent-table'] = lexer.get_token()
+                    assert lexer.get_token() == '('
+                    col['foreign-key']['parent-column'] = lexer.get_token()
+                    assert lexer.get_token() == ')'
+
+                    edge_lines.append("""    %s:%s:n -> %s:%s:s;""" % \
+                        (table['name'], child_column,
+                        col['foreign-key']['parent-table'],
+                        col['foreign-key']['parent-column']))
+
+                    col['foreign-key']['options'] = list()
                     tok = lexer.get_token()
-                    if tok.lower() == 'foreign':
-                        assert lexer.get_token().lower() == 'key'
-                        assert lexer.get_token() == '('
-                        child_column = lexer.get_token()
-                        col = table['columns'][child_column]
-                        col['foreign-key'] = dict()
-                        assert lexer.get_token() == ')'
-                        assert lexer.get_token().lower() == 'references'
-                        col['foreign-key']['parent-table'] = lexer.get_token()
-                        assert lexer.get_token() == '('
-                        col['foreign-key']['parent-column'] = lexer.get_token()
-                        assert lexer.get_token() == ')'
-
-                        edge_lines.append("""    %s:%s:n -> %s:%s:s;""" % \
-                            (table['name'], child_column,
-                            col['foreign-key']['parent-table'],
-                            col['foreign-key']['parent-column']))
-
-                        col['foreign-key']['options'] = list()
+                    while tok != ';' and tok != ',':
+                        col['foreign-key']['options'].append(tok)
                         tok = lexer.get_token()
-                        while tok != ';' and tok != ',':
-                            col['foreign-key']['options'].append(tok)
-                            tok = lexer.get_token()
-                    elif tok.lower() == 'primary':
-                        assert lexer.get_token().lower() == 'key'
-                        assert lexer.get_token() == '('
-                        while True:
-                            col = table['columns'][lexer.get_token()]
-                            col['primary-key'] = True
-                            tok = lexer.get_token()
-                            if tok != ',':
-                                break
-                        assert tok == ')'
-                        #tok = lexer.get_token()
-                        #if tok == '(':
-                            #while tok != ')':
-                                #tok = lexer.get_token()
+                elif tok.lower() == 'primary':
+                    assert lexer.get_token().lower() == 'key'
+                    assert lexer.get_token() == '('
+                    while True:
+                        col = table['columns'][lexer.get_token()]
+                        col['primary-key'] = True
                         tok = lexer.get_token()
-                        #assert tok == ',' or tok == ')'
-                    elif tok != '':
-                        col = dict(options=[])
-                        table['columns'][tok] = col
-                        tok = lexer.get_token()
-                        while tok != ',' and tok != ')':
-                            if tok.lower() == 'primary':
-                                assert lexer.get_token().lower() == 'key'
-                                col['primary-key'] = True
-                            col['options'].append(tok)
-                            tok = lexer.get_token()
-                        if tok == ')':
-                            tok = lexer.get_token()
-                            assert tok == ';'
+                        if tok != ',':
                             break
-                    else:
+                    assert tok == ')'
+                    #tok = lexer.get_token()
+                    #if tok == '(':
+                        #while tok != ')':
+                            #tok = lexer.get_token()
+                    tok = lexer.get_token()
+                    #assert tok == ',' or tok == ')'
+                elif tok != '':
+                    col = dict(options=[])
+                    table['columns'][tok] = col
+                    tok = lexer.get_token()
+                    while tok != ',' and tok != ')':
+                        if tok.lower() == 'primary':
+                            assert lexer.get_token().lower() == 'key'
+                            col['primary-key'] = True
+                        col['options'].append(tok)
+                        tok = lexer.get_token()
+                    if tok == ')':
+                        tok = lexer.get_token()
+                        assert tok == ';'
                         break
+                else:
+                    break
 
 for table in tables:
     dot_lines.append("""    %s[label=<
