e1cdb38e55568e510460348e9720c0e2004e07b8
Ryan Helinski <rlhelinski@gmail.com>
2018-06-28 08:02:04 -0600

    Drop copy of progressbar. Add requirements to setup()

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@
 
 from distutils.core import setup
 
-# TODO move this __version__ variable to mbcat/__init__.py
-from mbcat.catalog import __version__
 setup(name='mbcat',
-      version=__version__,
+      version='0.3',
       description='MusicBrainz Catalog',
       author='Ryan Helinski',
       # author_email='',
       url='https://www.github.com/rlhelinski/musicbrainz-catalog',
-      packages=['mbcat', 'progressbar'],
+      packages=['mbcat'],
       package_data={'mbcat': ['art/*.png', 'art/*.svg']},
       scripts=['mbcat-cmd.py', 'mbcat-gtk.py',
           'scripts/mbcat-add-disc', 'scripts/mbcat-disc-get-release'],
+      install_requires=[
+          'pygtk==2.24',
+          'musicbrainzngs==0.6',
+          'discid==1.1.1',
+          'progressbar==2.3'],
       )
